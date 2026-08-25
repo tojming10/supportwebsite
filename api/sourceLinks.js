dbd2d666ec5dd528590ded4b31c40989ef8b024d
tojming10 <tojming10@users.noreply.github.com>
2026-08-25 11:04:07 +0800

    Add DERYAN terms source

--- api/sourceLinks.js
+++ api/sourceLinks.js
@@ -1,5 +1,6 @@
 const DEFAULT_REFERENCE_LINKS = [
   // Add support article URLs here when you want the live site to use them.
+  "https://en.deryan.com/pages/algemene-voorwaarden",
 ];
 
 module.exports = { DEFAULT_REFERENCE_LINKS };
